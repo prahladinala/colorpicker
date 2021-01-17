2f42c6f166c33f4825c83e724004c38842de0d73
prahladinala <prahladinala@users.noreply.github.com>
2021-01-17 07:11:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var clipboard = new ClipboardJS('.box')
+
+clipboard.on('success', function (e) {
+  e.clearSelection()
+  e.trigger.textContent = 'Copied!'
+  var interval = setInterval(timer, 2000)
+
+  function timer() {
+    e.trigger.textContent = e.text
+    clearInterval(interval)
+  }
+})
+
+clipboard.on('error', function (e) {
+  console.info(e)
+})
