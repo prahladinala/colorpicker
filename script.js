6f537a114a562e5fc5a919eb935285fefa329d0c
prahladinala <prahladinala@users.noreply.github.com>
2021-01-17 08:06:28 +0530

    Updated Copy Function

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-var clipboard = new ClipboardJS('.box')
+var clipboard = new ClipboardJS('.btn');
 
 clipboard.on('success', function (e) {
-  e.clearSelection()
-  e.trigger.textContent = 'Copied!'
-  var interval = setInterval(timer, 2000)
+  e.clearSelection();
+  e.trigger.textContent = 'Copied!';
+  var interval = setInterval(timer, 2000);
 
   function timer() {
-    e.trigger.textContent = e.text
-    clearInterval(interval)
+    e.trigger.textContent = e.text;
+    clearInterval(interval);
   }
-})
+
+
+
+});
 
 clipboard.on('error', function (e) {
-  console.info(e)
-})
+  console.info(e);
+});
